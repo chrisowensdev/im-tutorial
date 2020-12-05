fdb8931b62cc252fdc084e6dc9ee09f6d7ba96f8
Chris Owens <guitarrockstarco@gmail.com>
2020-12-05 18:36:10 -0500

    Add styles for signup component

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import {
+    FormControl,
+    InputLabel,
+    Input,
+    Paper,
+    withStyles,
+    CssBaseline,
+    Typography,
+    Button,
+} from '@material-ui/core';
+import styles from './styles';
+const firebase = require('firebase');
 
 class Signup extends React.Component {
     render() {
