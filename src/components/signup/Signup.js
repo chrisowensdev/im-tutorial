8cf1827784de64ea7802285d54b2ba0692217dbd
Chris Owens <guitarrockstarco@gmail.com>
2020-12-07 19:53:14 -0500

    Email Controlled Input

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -19,10 +19,38 @@ class Signup extends React.Component {
         return (
             <main className={classes.main}>
                 <CssBaseline></CssBaseline>
-                <Paper className={classes.paper}></Paper>
+                <Paper className={classes.paper}>
+                    <Typography component='h1' variant='h5'>
+                        Sign Up!
+                    </Typography>
+                    <form
+                        onSubmit={(e) => this.submitSignup(e)}
+                        className={classes.form}
+                    >
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='signup-email-input'>
+                                Enter Your Email
+                            </InputLabel>
+                            <Input
+                                autoComplete='email'
+                                onChange={(e) => this.userTyping('email', e)}
+                                autoFocus
+                                id='signup-email-input'
+                            ></Input>
+                        </FormControl>
+                    </form>
+                </Paper>
             </main>
         );
     }
+
+    userTyping = (type, e) => {
+        console.log(type, e);
+    };
+
+    submitSignup = (e) => {
+        console.log('Submitting');
+    };
 }
 
 export default withStyles(styles)(Signup);
